308f9a34ee5ef8a95d602f53c52296c073db169a
Erik Barroso <coderik1996@gmail.com>
2024-01-25 17:04:03 -0300

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import City from "./assets/city.jpg"
+import ManageData from './components/ManageData';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
           <img src="/img1.jpg" alt="" />
           <img src={City} alt="" />
         </div>
+        <ManageData />
       </div>
     </>
   );
