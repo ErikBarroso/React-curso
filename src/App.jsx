4eda9e4abc989c4b5347e8bb2146760e057c54eb
Erik Barroso <coderik1996@gmail.com>
2024-01-26 11:14:16 -0300

    Destruction e props

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
 import './App.css'
 import City from "./assets/city.jpg"
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
+import ShowUserNAme from './components/ShowUserNAme';
+import CarDetails from './components/CarDetails';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <h1>Avançando em React</h1>
         <div>
-          {/* <img src="/img1.jpg" alt="" />
-          <img src={City} alt="" /> */}
+          {/* {/* <img src="/img1.jpg" alt="" /> */}
+          {/* <img src={City} alt="" /> */}
         </div>
         {/* <ManageData /> */}
-        {/* <ListRender/> */}
-        {<ConditionalRender/>}
+        {/* <ListRender /> */}
+        {/* {<ConditionalRender />} */}
+        {/* {<ShowUserNAme name="Erik" />} */}
+        <CarDetails brand = "teste" km = {3000} color = 'Red' />
       </div>
     </>
   );
---
+++ src/components/ShowUserNAme.jsx
@@ -0,0 +1,9 @@
+function ShowUserNAme({name}) {
+  return (
+    <div>
+      <h2>O nome é: {name}</h2>
+    </div>
+  )
+}
+
+export default ShowUserNAme
