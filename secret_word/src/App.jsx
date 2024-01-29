55b5ddd719e9246f8706ed21836eea0a3e3b6af3
Erik Barroso <coderik1996@gmail.com>
2024-01-29 12:58:11 -0300

    consumindo states do jogo

--- secret_word/src/App.jsx
+++ secret_word/src/App.jsx
@@ -18,6 +18,12 @@ function App() {
     const [pickedCategory, setPickedCategory] = useState("") 
     const [letters, setLetters] = useState(""); 
 
+    const[guessedLetters, setGuessedLetters] = useState([])
+    const [wrongLetters, setWrongLetters] = useState([]);
+    const [guesses, setGguesses] = useState(3);
+    const [score, setScore] = useState(0);
+
+
     const pickWordAndCategory = () => {
       const categories = Object.keys(words)
       const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
@@ -44,7 +50,17 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "game" && 
+      <Game 
+      verifyLetter={verifyLetter} 
+      pickedWord={pickedWord} 
+      pickedCategory={pickedCategory} 
+      letters = { letters} 
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters}
+      guesses={guesses}
+      score={score}
+      />}
       {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
