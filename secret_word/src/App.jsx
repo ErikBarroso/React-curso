2f228c6ce5d2387d706b10d91d7d2cdf94cd3474
Erik Barroso <coderik1996@gmail.com>
2024-01-29 10:38:42 -0300

    criando componentes do jogo

--- secret_word/src/App.jsx
+++ secret_word/src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './App.css'
 import StartScreen from './components/StartScreen'
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+import { wordsList } from './data/words';
 
+ const stages = [
+   { id: 1, name: "start" },
+   { id: 2, name: "game" },
+   { id: 3, name: "end" },
+ ];
 function App() {
-
+    const [gameStage, setGameStage] = useState(stages[0].name)
+    const [words] = useState(wordsList)
+    console.log(words);
   return (
-      <div className='App'>
-        < StartScreen />
-      </div>
-  )
+    <div className="App">
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
+    </div>
+  );
 }
 
 export default App
