2f0d2e3ae17888bad4ddcc802a6e82c83ed88533
Erik Barroso <coderik1996@gmail.com>
2024-01-29 06:36:19 -0300

    textarea = texto maiores

--- form_react/src/components/MyForm.jsx
+++ form_react/src/components/MyForm.jsx
@@ -5,6 +5,7 @@ const MyForm = ({user}) => {
   // Gerenciamento de dados
   const [name, setName] = useState(user ? user.name : "")
   const [email, setEmail] = useState(user ? user.email : "");
+  const [bio, setBio] = useState("")
 
   const handleName= (e) =>{
     //mudamos o estado da variável cada vez que digitamos algo no input
@@ -15,19 +16,36 @@ const MyForm = ({user}) => {
     event.preventDefault() //form n recarregar mas a página
     setName("")
     setEmail("")
+    setBio("");
+    console.log(name, email, bio);
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName} value={name}/>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+            onChange={handleName}
+            value={name}
+          />
         </div>
         {/* Forma sugerida pela doc do react */}
         <label>
           <span>E-mail:</span>
-          <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)} value={email}/>
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
         </label>
+        {/* textrea */}
+        <span>Bio:</span>
+        <textarea name="bio" placeholder="Descrição do usuário" onChange={(e) => setBio(e.target.value)} value ={bio}></textarea>
         <input type="submit" value="Enviar" />
       </form>
     </div>
